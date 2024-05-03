217ec91532f40dc052aa155b178c2551d5bf3846
Mikhail <newlevelcenter@gmail.com>
2024-05-03 09:13:34 +0300

    week

---
+++ ERP/code_examples/take_the_data_by_week.py
@@ -0,0 +1,152 @@
+Monday = [
+    {"time":"7:00","action":"Morning exercises","spheare":"Health"},
+    {"time":"8:00","action":"Finance planning","spheare":"Finances"},
+    {"time":"9:00","action":"Week planning","spheare":"Career"},
+    {"time":"10:00","action":"Report preparation","spheare":"Career"},
+    {"time":"11:00","action":"Report preparation","spheare":"Career"},
+    {"time":"12:00","action":"Weekly meeting","spheare":"Career"},
+    {"time":"13:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"14:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"15:00","action":"Language study","spheare":"Self-development"},
+    {"time":"16:00","action":"Language study","spheare":"Self-development"},
+    {"time":"17:00","action":"Master class for Product management","spheare":"Self-development"},
+    {"time":"18:00","action":"Time with family","spheare":"Family"},
+    {"time":"19:00","action":"Time with family","spheare":"Family"},
+    {"time":"20:00","action":"Football","spheare":"Health"},
+    {"time":"21:00","action":"Football","spheare":"Health"},
+    {"time":"22:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"23:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"00:00","action":"","spheare":"Free"}
+]
+Tuesday = [
+    {"time":"7:00","action":"Morning exercises","spheare":"Health"},
+    {"time":"8:00","action":"Finance planning","spheare":"Finances"},
+    {"time":"9:00","action":"Week planning","spheare":"Career"},
+    {"time":"10:00","action":"Report preparation","spheare":"Career"},
+    {"time":"11:00","action":"Report preparation","spheare":"Career"},
+    {"time":"12:00","action":"Weekly meeting","spheare":"Career"},
+    {"time":"13:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"14:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"15:00","action":"Language study","spheare":"Self-development"},
+    {"time":"16:00","action":"Language study","spheare":"Self-development"},
+    {"time":"17:00","action":"Master class for Product management","spheare":"Self-development"},
+    {"time":"18:00","action":"Time with family","spheare":"Family"},
+    {"time":"19:00","action":"Time with family","spheare":"Family"},
+    {"time":"20:00","action":"Football","spheare":"Health"},
+    {"time":"21:00","action":"Football","spheare":"Health"},
+    {"time":"22:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"23:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"00:00","action":"","spheare":"Free"}
+]
+Wednesday = [
+    {"time":"7:00","action":"Morning exercises","spheare":"Health"},
+    {"time":"8:00","action":"Finance planning","spheare":"Finances"},
+    {"time":"9:00","action":"Week planning","spheare":"Career"},
+    {"time":"10:00","action":"Report preparation","spheare":"Career"},
+    {"time":"11:00","action":"Report preparation","spheare":"Career"},
+    {"time":"12:00","action":"Weekly meeting","spheare":"Career"},
+    {"time":"13:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"14:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"15:00","action":"Language study","spheare":"Self-development"},
+    {"time":"16:00","action":"Language study","spheare":"Self-development"},
+    {"time":"17:00","action":"Master class for Product management","spheare":"Self-development"},
+    {"time":"18:00","action":"Time with family","spheare":"Family"},
+    {"time":"19:00","action":"Time with family","spheare":"Family"},
+    {"time":"20:00","action":"Football","spheare":"Health"},
+    {"time":"21:00","action":"Football","spheare":"Health"},
+    {"time":"22:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"23:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"00:00","action":"","spheare":"Free"}
+]
+Thursday =[
+    {"time":"7:00","action":"Morning exercises","spheare":"Health"},
+    {"time":"8:00","action":"Finance planning","spheare":"Finances"},
+    {"time":"9:00","action":"Week planning","spheare":"Career"},
+    {"time":"10:00","action":"Report preparation","spheare":"Career"},
+    {"time":"11:00","action":"Report preparation","spheare":"Career"},
+    {"time":"12:00","action":"Weekly meeting","spheare":"Career"},
+    {"time":"13:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"14:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"15:00","action":"Language study","spheare":"Self-development"},
+    {"time":"16:00","action":"Language study","spheare":"Self-development"},
+    {"time":"17:00","action":"Master class for Product management","spheare":"Self-development"},
+    {"time":"18:00","action":"Time with family","spheare":"Family"},
+    {"time":"19:00","action":"Time with family","spheare":"Family"},
+    {"time":"20:00","action":"Football","spheare":"Health"},
+    {"time":"21:00","action":"Football","spheare":"Health"},
+    {"time":"22:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"23:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"00:00","action":"","spheare":"Free"}
+]
+
+Friday = [
+    {"time":"7:00","action":"Morning exercises","spheare":"Health"},
+    {"time":"8:00","action":"Finance planning","spheare":"Finances"},
+    {"time":"9:00","action":"Week planning","spheare":"Career"},
+    {"time":"10:00","action":"Report preparation","spheare":"Career"},
+    {"time":"11:00","action":"Report preparation","spheare":"Career"},
+    {"time":"12:00","action":"Weekly meeting","spheare":"Career"},
+    {"time":"13:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"14:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"15:00","action":"Language study","spheare":"Self-development"},
+    {"time":"16:00","action":"Language study","spheare":"Self-development"},
+    {"time":"17:00","action":"Master class for Product management","spheare":"Self-development"},
+    {"time":"18:00","action":"Time with family","spheare":"Family"},
+    {"time":"19:00","action":"Time with family","spheare":"Family"},
+    {"time":"20:00","action":"Football","spheare":"Health"},
+    {"time":"21:00","action":"Football","spheare":"Health"},
+    {"time":"22:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"23:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"00:00","action":"","spheare":"Free"}
+]
+Saturday = [
+    {"time":"7:00","action":"Morning exercises","spheare":"Health"},
+    {"time":"8:00","action":"Finance planning","spheare":"Finances"},
+    {"time":"9:00","action":"Week planning","spheare":"Career"},
+    {"time":"10:00","action":"Report preparation","spheare":"Career"},
+    {"time":"11:00","action":"Report preparation","spheare":"Career"},
+    {"time":"12:00","action":"Weekly meeting","spheare":"Career"},
+    {"time":"13:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"14:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"15:00","action":"Language study","spheare":"Self-development"},
+    {"time":"16:00","action":"Language study","spheare":"Self-development"},
+    {"time":"17:00","action":"Master class for Product management","spheare":"Self-development"},
+    {"time":"18:00","action":"Time with family","spheare":"Family"},
+    {"time":"19:00","action":"Time with family","spheare":"Family"},
+    {"time":"20:00","action":"Football","spheare":"Health"},
+    {"time":"21:00","action":"Football","spheare":"Health"},
+    {"time":"22:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"23:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"00:00","action":"","spheare":"Free"}
+]
+Sunday =  [
+    {"time":"7:00","action":"Morning exercises","spheare":"Health"},
+    {"time":"8:00","action":"Finance planning","spheare":"Finances"},
+    {"time":"9:00","action":"Week planning","spheare":"Career"},
+    {"time":"10:00","action":"Report preparation","spheare":"Career"},
+    {"time":"11:00","action":"Report preparation","spheare":"Career"},
+    {"time":"12:00","action":"Weekly meeting","spheare":"Career"},
+    {"time":"13:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"14:00","action":"Math of Economics","spheare":"Career"},
+    {"time":"15:00","action":"Language study","spheare":"Self-development"},
+    {"time":"16:00","action":"Language study","spheare":"Self-development"},
+    {"time":"17:00","action":"Master class for Product management","spheare":"Self-development"},
+    {"time":"18:00","action":"Time with family","spheare":"Family"},
+    {"time":"19:00","action":"Time with family","spheare":"Family"},
+    {"time":"20:00","action":"Football","spheare":"Health"},
+    {"time":"21:00","action":"Football","spheare":"Health"},
+    {"time":"22:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"23:00","action":"Bar after football","spheare":"Environment"},
+    {"time":"00:00","action":"","spheare":"Free"}
+]
+# how many time we spend to Career on Monday
+week = [Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday]
+day_number = 0
+count = 0
+for day in week:
+  for time in day:
+    if day_number == 0 and time["spheare"]=="Career":
+       count+=1
+    
+  day_number =+1
+print(count)
